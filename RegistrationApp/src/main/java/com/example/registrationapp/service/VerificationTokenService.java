a3ca52f901f572c3c4ebad5cfd931a63f8993af4
hoangtuananh <hoangtuananh1997tb@gmail.com>
2022-07-23 10:06:30 +0700

    update

--- RegistrationApp/src/main/java/com/example/registrationapp/service/VerificationTokenService.java
+++ RegistrationApp/src/main/java/com/example/registrationapp/service/VerificationTokenService.java
@@ -3,7 +3,12 @@ package com.example.registrationapp.service;
 import com.example.registrationapp.persistence.dao.VerificationTokenRepository;
 import com.example.registrationapp.persistence.model.VerificationToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class VerificationTokenService implements IVerificationTokenService{
     @Autowired
     private VerificationTokenRepository verificationTokenRepository;
